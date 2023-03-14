4e8982bfb10d624e220e3c2e718e42233c5e3e72
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-03-14 15:56:42 -0500

    Reformat inputs and layout

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 import "./style.css";
 
-console.log("Hello World");
+const form = document.querySelector("form");
+const email = document.getElementById("email");
+const country = document.getElementById("country");
+const zipCode = document.getElementById("zipCode");
+const password = document.getElementById("password");
+const confirm = document.getElementById("confirm");
+const emailError = document.querySelector("#email + span.error");
+
+email.addEventListener("input", (event) => {
+  if (email.validity.valid) {
+    emailError.textContent = ""; // Reset the content of the message
+    emailError.className = "error"; // Reset the visual state of the message
+  } else {
+    showError();
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  if (!email.validity.valid) {
+    showError();
+    event.preventDefault();
+  }
+});
+
+function showError() {
+  if (email.validity.valueMissing) {
+    // If the field is empty,
+    // display the following error message.
+    emailError.textContent = "You need to enter an email address.";
+  } else if (email.validity.typeMismatch) {
+    // If the field doesn't contain an email address,
+    // display the following error message.
+    emailError.textContent = "Entered value needs to be an email address.";
+  } else if (email.validity.tooShort) {
+    // If the data is too short,
+    // display the following error message.
+    emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+  }
+
+  // Set the styling appropriately
+  emailError.className = "error active";
+}
