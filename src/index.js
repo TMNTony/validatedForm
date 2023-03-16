380bec430a745551fa1f3ed3955803370def8fb6
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-03-16 11:35:25 -0500

    Fix regex and debugged listeners

--- src/index.js
+++ src/index.js
@@ -2,29 +2,15 @@ import "./style.css";
 
 const form = document.querySelector("form");
 const email = document.getElementById("email");
-const country = document.getElementById("country");
 const zipCode = document.getElementById("zipCode");
 const password = document.getElementById("password");
 const confirm = document.getElementById("confirm");
 const emailError = document.querySelector("#email + span.error");
+const zipCodeError = document.querySelector("#zipCode + span.error");
+const passwordError = document.querySelector("#password + span.error");
+const confirmError = document.querySelector("#confirm + span.error");
 
-email.addEventListener("input", (event) => {
-  if (email.validity.valid) {
-    emailError.textContent = ""; // Reset the content of the message
-    emailError.className = "error"; // Reset the visual state of the message
-  } else {
-    showError();
-  }
-});
-
-form.addEventListener("submit", (event) => {
-  if (!email.validity.valid) {
-    showError();
-    event.preventDefault();
-  }
-});
-
-function showError() {
+function showEmailError() {
   if (email.validity.valueMissing) {
     // If the field is empty,
     // display the following error message.
@@ -42,3 +28,75 @@ function showError() {
   // Set the styling appropriately
   emailError.className = "error active";
 }
+
+function showZipCodeError() {
+  if (zipCode.validity.valueMissing) {
+    zipCodeError.textContent = "You need to enter a zip code.";
+  } else {
+    zipCodeError.textContent = "Entered value needs to be a valid zip code.";
+  }
+  zipCodeError.className = "error active";
+}
+
+function showPasswordError() {
+  if (password.validity.valueMissing) {
+    passwordError.textContent = "You need to enter a password.";
+  } else {
+    passwordError.textContent = "Password must be 8-12 characters and contain uppercase, lowercase, numbers, and specials characters.";
+  }
+  passwordError.className = "error active";
+}
+
+function showConfirmError() {
+  if (confirm.validity.valueMissing) {
+    confirmError.textContent = "You need to enter a confirm.";
+  } else if (confirm.value !== password.value) {
+    confirmError.textContent = "Confirm value does not match password.";
+  } else {
+    confirmError.textContent = "";
+  }
+  confirmError.className = "error active";
+}
+
+email.addEventListener("input", (event) => {
+  if (email.validity.valid) {
+    emailError.textContent = ""; // Reset the content of the message
+    emailError.className = "error"; // Reset the visual state of the message
+  } else {
+    showEmailError();
+  }
+});
+
+zipCode.addEventListener("input", (event) => {
+  if (zipCode.validity.valid) {
+    zipCodeError.textContent = ""; // Reset the content of the message
+    zipCodeError.className = "error"; // Reset the visual state of the message
+  } else {
+    showZipCodeError();
+  }
+});
+
+password.addEventListener("input", (event) => {
+  if (password.validity.valid) {
+    passwordError.textContent = ""; // Reset the content of the message
+    passwordError.className = "error"; // Reset the visual state of the message
+  } else {
+    showPasswordError();
+  }
+});
+
+confirm.addEventListener("input", (event) => {
+  if (confirm.validity.valid) {
+    confirmError.textContent = ""; // Reset the content of the message
+    confirmError.className = "error"; // Reset the visual state of the message
+  } else {
+    showConfirmError();
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  if (!email.validity.valid) {
+    showEmailError();
+    event.preventDefault();
+  }
+});
